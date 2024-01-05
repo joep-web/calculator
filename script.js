28b5d0692e86ac81d4dd0c7d101e11ae521fdee4
johnjoepreydangkulos@gmail.com <johnjoepreydangkulos@gmail.com>
2024-01-05 16:51:17 +0800

    new

--- script.js
+++ script.js
@@ -1,25 +1,102 @@
-const buttons = document.querySelectorAll('input[type="button"]');
-const display = document.querySelector('.display');
-const specialChars = ["%", "+", "-", "x", "÷", "="];
-let output = "";
 
-const calculate = (btnVal) => {
-  
-  if (btnVal === "=" && output !== "") {
-     output = eval(output.replace("%", "/100").replace(/x/g, '*').replace(/÷/g, '/'));
-  } else if (btnVal === "AC"){
-    output = "";
-  } else if (btnVal === "Del") {
-    output = output.toString().slice(0, -1);
-  } else {
-    if (output === "" && specialChars.includes(btnVal)) return;
-    output += btnVal;
-  }
-  display.value = output;
+const number = document.querySelectorAll("[data-num]");
+const operator = document.querySelectorAll("[data-operator]");
+const currNum = document.querySelector(".curr-num");
+const prevNum = document.querySelector(".prev-num")
+const AC = document.querySelector("#clear");
+const DEL = document.querySelector("#del");
+const EQ = document.querySelector("[data-equals]");
+const PERCENT = document.querySelector("[data-percent]");
+let cN = "";
+let pN = "";
+let op = undefined;
+
+const solve = () => {
+    let result;
+    const prev = parseFloat(pN);
+    const curr = parseFloat(cN);
+    if(isNaN(prev) || isNaN(curr))return;
+    
+    
+    switch(op) {
+        case "+":
+            result = prev + curr;
+            break;
+        case "-":
+            result = prev - curr;
+            break;
+        case "x":
+            result = prev * curr;
+            break;
+        case "÷":
+            result = prev / curr;
+            break;
+        default:
+            return;
+    }
+    cN = result;
+    op = "";
+    pN = "";
+}
+
+const numAppend = (num) => {
+  if(num === "." && currNum.innerText.includes(".")) return;
+  cN += num;
 };
 
-buttons.forEach((button) => {
-  button.addEventListener('click', (event) => {
-  calculate(event.target.value);
+const chooseOp = (ope) => {
+    if(cN === "") return;
+    if(pN !== "") {
+        solve();
+    }
+    op = ope;
+    pN = cN;
+    cN = "";
+}
+
+
+const updateDisplay = () => {
+    currNum.innerText = cN;
+    if(op != null) {
+        prevNum.innerText = `${pN} ${op}`;
+    }
+}
+
+number.forEach(num => {
+  num.addEventListener("click", (e) => {
+    numAppend(e.target.value);
+    updateDisplay();
   });
+});
+
+operator.forEach(op => {
+  op.addEventListener("click", (e) => {
+    chooseOp(e.target.value);
+    updateDisplay();
+  });
+});
+
+AC.addEventListener("click", () => {
+  cN= "";
+  pN = "";
+  op = "";
+  updateDisplay();
+});
+
+DEL.addEventListener("click", () => {
+  if(!Number.isInteger(cN)){
+      cN = String(cN);
+  }
+  cN = cN.slice(0, -1);
+  updateDisplay();
+});;
+
+EQ.addEventListener("click", () => {
+    solve();
+    updateDisplay();
+});
+
+PERCENT.addEventListener("click", () => {
+    cN = parseFloat(cN) / 100;
+    updateDisplay();
 });
